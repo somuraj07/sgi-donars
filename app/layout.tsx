16f49d075a77747a8e922a2686b4b7f303962e75
somuraj07 <somasankar33@gmail.com>
2025-08-02 17:19:45 +0530

    toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Navbar from "@/components/Navbar";
 import Footer from "@/components/Footer";
+import { Toaster } from "react-hot-toast";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -33,6 +34,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <Navbar/>
+        <Toaster position="top-right" reverseOrder={false} />
         <main>
           {children}
         </main>
